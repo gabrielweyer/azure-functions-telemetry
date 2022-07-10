b5506d86d8e4d2e5f0b6ead797f0a15b80719905
Gabriel Weyer <gabrielweyer@gmail.com>
2022-07-10 20:29:34 +1000

    Restructure tests
    Need to make some room for the Service Bus tests!

---
+++ tests/AzureFunctionsTelemetryIntegrationTests/DefaultHttpTests.cs
@@ -0,0 +1,69 @@
+using FluentAssertions;
+using Gabo.AzureFunctionsTelemetryIntegrationTests.TestInfrastructure;
+using Xunit;
+
+namespace Gabo.AzureFunctionsTelemetryIntegrationTests;
+
+public class DefaultHttpTests
+{
+    private static readonly DefaultTelemetryFunctionClient _client;
+
+    static DefaultHttpTests()
+    {
+        _client = new DefaultTelemetryFunctionClient();
+    }
+
+    [Fact]
+    public async Task GivenHttpRequest_ThenTwoExecutionTracesAreEmitted()
+    {
+        // Arrange
+        await _client.DeleteTelemetryAsync();
+
+        // Act
+        await _client.LogVerboseAsync();
+
+        // Assert
+        var telemetryItems = await _client.GetTelemetryAsync();
+        var requestTelemetry = telemetryItems.Should()
+            .ContainSingle(i => i.Name == "AppRequests" && i.Tags.OperationName == "TraceLogFunction").Subject;
+        var executionTraces = telemetryItems
+            .Where(i => i.Name == "AppTraces" && i.Tags.OperationId == requestTelemetry.Tags.OperationId)
+            .ToList();
+        executionTraces.Should().HaveCount(2);
+    }
+
+    [Fact]
+    public async Task GivenHttpBinding_WhenException_ThenDuplicateExceptionTelemetry()
+    {
+        // Arrange
+        await _client.DeleteTelemetryAsync();
+
+        // Act
+        await _client.ThrowOnHttpBindingAsync();
+
+        // Assert
+        var telemetryItems = await _client.GetTelemetryAsync();
+        var requestTelemetry = telemetryItems.Should()
+            .ContainSingle(i => i.Name == "AppRequests" && i.Tags.OperationName == "HttpExceptionThrowingFunction").Subject;
+        var exceptions = telemetryItems
+            .Where(i => i.Name == "AppExceptions" && i.Tags.OperationId == requestTelemetry.Tags.OperationId)
+            .ToList();
+        exceptions.Should().HaveCount(2);
+    }
+
+    [Fact]
+    public async Task GivenHealthCheck_ThenRequestIsNotDiscarded()
+    {
+        // Arrange
+        await _client.DeleteTelemetryAsync();
+
+        // Act
+        await _client.CheckHealthAsync();
+        await _client.LogVerboseAsync();
+
+        // Assert
+        var telemetryItems = await _client.GetTelemetryAsync();
+        telemetryItems.Should().Contain(i => i.Name == "AppRequests" && i.Tags.OperationName == "HealthFunction");
+        telemetryItems.Should().Contain(i => i.Name == "AppRequests" && i.Tags.OperationName == "TraceLogFunction");
+    }
+}

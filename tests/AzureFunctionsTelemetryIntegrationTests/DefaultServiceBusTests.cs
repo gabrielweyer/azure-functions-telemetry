eec5895a83907d4e12fb47c481a08188a227a955
Gabriel Weyer <gabrielweyer@gmail.com>
2022-07-10 22:52:22 +1000

    Test discarding Service Bus trigger traces

--- tests/AzureFunctionsTelemetryIntegrationTests/DefaultServiceBusTests.cs
+++ tests/AzureFunctionsTelemetryIntegrationTests/DefaultServiceBusTests.cs
@@ -24,4 +24,20 @@ public class DefaultServiceBusTests
         Assert.Null(request.Data.BaseData.Url);
         request.Data.BaseData.ResponseCode.Should().Be("0");
     }
+
+    [Fact]
+    public async Task GivenServiceBusRequest_ThenThreeExecutionTracesAreEmitted()
+    {
+        // Arrange
+        await _client.DeleteTelemetryAsync();
+
+        // Act
+        await _client.TriggerServiceBusAsync();
+
+        // Assert
+        var (request, telemetries) =
+            await _client.PollForTelemetryAsync<RequestItem>(i => i.OperationName == "ServiceBusFunction");
+        var executionTraces = telemetries.GetOperationItems<TraceItem>(request.OperationId);
+        executionTraces.Should().HaveCount(3);
+    }
 }
--- tests/AzureFunctionsTelemetryIntegrationTests/TestInfrastructure/TraceItem.cs
+++ tests/AzureFunctionsTelemetryIntegrationTests/TestInfrastructure/TraceItem.cs
@@ -2,5 +2,15 @@ namespace Gabo.AzureFunctionsTelemetryIntegrationTests.TestInfrastructure;
 
 internal class TraceItem : TelemetryItem
 {
+    public TraceData Data { get; set; } = new();
+}
 
+internal class TraceData
+{
+    public TraceBaseData BaseData { get; set; } = new();
+}
+
+internal class TraceBaseData
+{
+    public string Message { get; set; } = default!;
 }

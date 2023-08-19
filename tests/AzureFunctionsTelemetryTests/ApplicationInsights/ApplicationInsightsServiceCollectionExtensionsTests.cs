23a0af001bce1dc48cd97c8ee773a418260652bc
Gabriel Weyer <gabrielweyer@gmail.com>
2023-08-19 15:56:24 +1000

    Make Cloud role name and Application version optional

---
+++ tests/AzureFunctionsTelemetryTests/ApplicationInsights/ApplicationInsightsServiceCollectionExtensionsTests.cs
@@ -0,0 +1,45 @@
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gabo.AzureFunctionsTelemetryTests.ApplicationInsights;
+
+public class ApplicationInsightsServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void GivenApplicationNameNotProvided_ThenDontRegisterApplicationInitializer()
+    {
+        // Arrange
+        var serviceCollection = new ServiceCollection();
+        var config = new CustomApplicationInsightsConfig(applicationName: null, typeof(StringHelper), "Q");
+
+        // Act
+        var updatedServiceCollection = serviceCollection.AddCustomApplicationInsights(config);
+
+        // Assert
+        var registeredTelemetryInitializers = GetTelemetryInitializers(updatedServiceCollection);
+        registeredTelemetryInitializers.Should().NotContain(typeof(ApplicationInitializer));
+    }
+
+    [Fact]
+    public void GivenApplicationNameProvided_ThenRegisterApplicationInitializer()
+    {
+        // Arrange
+        var serviceCollection = new ServiceCollection();
+        var config = new CustomApplicationInsightsConfig("app-name", typeof(StringHelper), "Q");
+
+        // Act
+        var updatedServiceCollection = serviceCollection.AddCustomApplicationInsights(config);
+
+        // Assert
+        var registeredTelemetryInitializers = GetTelemetryInitializers(updatedServiceCollection);
+        registeredTelemetryInitializers.Should().Contain(typeof(ApplicationInitializer));
+    }
+
+    private static IReadOnlyList<Type> GetTelemetryInitializers(IServiceCollection serviceCollection)
+    {
+        return serviceCollection
+            .Where(s => s.ServiceType == typeof(ITelemetryInitializer))
+            .Select(s => s.ImplementationType)
+            .ToList();
+    }
+}
